3960dc11e936ef5701b7040e06e9b4cbd2e42369
Ivan Lee <ivanklee86@gmail.com>
2024-01-16 08:17:33 +0000

    Get mkdocs working!

--- .devcontainer/post_install.sh
+++ .devcontainer/post_install.sh
@@ -5,7 +5,7 @@ set -ex
 pip install -U pip poetry
 
 # Set up poetry
-poetry config cache-dir ${WORKSPACE_DIR}/.cache
+poetry config cache-dir /workspaces/lab/.cache
 poetry config virtualenvs.in-project true
 
 # Intall dependencies
