43b022d8142553342eda4b6fc885bb4c39843ddb
Ivan Lee <ivanklee86@gmail.com>
2024-01-16 03:18:57 -0500

    More tools setup (#5)
    * Initialize Python and add taskfile.
    
    * Setup.
    
    * Switch to vscode user.
    
    * Get mkdocs working!

--- .devcontainer/post_install.sh
+++ .devcontainer/post_install.sh
@@ -1,1 +1,12 @@
-echo "Hello lab!"
+#!/bin/bash
+set -ex
+
+# Install basic Python packages
+pip install -U pip poetry
+
+# Set up poetry
+poetry config cache-dir /workspaces/lab/.cache
+poetry config virtualenvs.in-project true
+
+# Intall dependencies
+poetry install
