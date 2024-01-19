fd539acdecec6e7bbebf8bfb8f1c8ba39826c9ae
Ivan Lee <ivanklee86@gmail.com>
2024-01-19 07:07:58 +0000

    Update configuration.

--- terraform/spacelift/stacks/main.tf
+++ terraform/spacelift/stacks/main.tf
@@ -1,15 +1,17 @@
 resource "spacelift_stack" "stacks" {
   for_each = toset(local.stacks)
 
-  administrative          = true
-  autodeploy              = true
-  branch                  = "master"
-  description             = each.value.description
-  labels                  = toset(each.value.labels)
-  name                    = each.value.name
-  project_root            = each.value.project_root
-  repository              = "lab"
-  space_id                = "root"
-  terraform_version       = "1.6.0"
-  terraform_workflow_tool = "OPEN_TOFU"
+  administrative               = true
+  autodeploy                   = true
+  branch                       = "master"
+  description                  = each.value.description
+  labels                       = toset(each.value.labels)
+  name                         = each.value.name
+  manage_state                 = true
+  project_root                 = each.value.project_root
+  repository                   = "lab"
+  space_id                     = "root"
+  terraform_version            = "1.6.0"
+  terraform_workflow_tool      = "OPEN_TOFU"
+  terraform_smart_sanitization = true
 }
