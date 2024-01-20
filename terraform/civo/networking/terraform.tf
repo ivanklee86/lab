2e0a646ec447564a0c25a359f69c9c9c0ee2f6bd
Ivan Lee <ivanklee86@gmail.com>
2024-01-20 07:54:17 +0000

    Add cloudflare provider.

--- terraform/civo/networking/terraform.tf
+++ terraform/civo/networking/terraform.tf
@@ -2,7 +2,11 @@ terraform {
   required_providers {
     civo = {
       source  = "civo/civo"
-      version = "1.0.39"
+      version = "~> 1.0"
+    }
+    cloudflare = {
+      source  = "cloudflare/cloudflare"
+      version = "~> 2.0"
     }
   }
 }
