2cb3125d8e5ca65cb0458222418e909d3b0f87a9
Ivan Lee <ivanklee86@gmail.com>
2024-01-20 12:34:36 +0000

    Linting fixes.

--- terraform/civo/kubernetes/main.tf
+++ terraform/civo/kubernetes/main.tf
@@ -1,9 +1,9 @@
 resource "civo_kubernetes_cluster" "lab" {
-  name         = "lab"
-  applications = ""
+  name               = "lab"
+  applications       = ""
   kubernetes_version = "1.28.2"
-  network_id   = data.terraform_remote_state.networking.outputs.network_id
-  firewall_id  = data.terraform_remote_state.networking.outputs.firewall_id
+  network_id         = data.terraform_remote_state.networking.outputs.network_id
+  firewall_id        = data.terraform_remote_state.networking.outputs.firewall_id
   pools {
     label      = "nodes"
     size       = element(data.civo_size.xsmall.sizes, 0).name
