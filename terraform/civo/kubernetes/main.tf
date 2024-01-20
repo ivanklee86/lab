382a3d346d08072f5b0f6fed7e1e168acc5eebd9
Ivan Lee <ivanklee86@gmail.com>
2024-01-20 05:20:49 -0500

    Configure network id for Kubernetes cluster. (#24)

--- terraform/civo/kubernetes/main.tf
+++ terraform/civo/kubernetes/main.tf
@@ -1,6 +1,7 @@
 resource "civo_kubernetes_cluster" "lab" {
   name         = "lab"
   applications = "-traefik2-loadbalancer"
+  network_id   = data.terraform_remote_state.networking.outputs.network_id
   firewall_id  = data.terraform_remote_state.networking.outputs.firewall_id
   pools {
     label      = "nodes"
