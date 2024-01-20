4a9085c3bdacfc28fb7c74678a8bba52727f8fd5
Ivan Lee <ivanklee86@gmail.com>
2024-01-20 07:35:38 -0500

    Specify Kubernetes version and no application. (#27)
    * Specify Kubernetes version and no application.
    
    * Linting fixes.

--- terraform/civo/kubernetes/main.tf
+++ terraform/civo/kubernetes/main.tf
@@ -1,8 +1,9 @@
 resource "civo_kubernetes_cluster" "lab" {
-  name         = "lab"
-  applications = "-traefik2-loadbalancer, -traefik2-nodeport"
-  network_id   = data.terraform_remote_state.networking.outputs.network_id
-  firewall_id  = data.terraform_remote_state.networking.outputs.firewall_id
+  name               = "lab"
+  applications       = ""
+  kubernetes_version = "1.28.2"
+  network_id         = data.terraform_remote_state.networking.outputs.network_id
+  firewall_id        = data.terraform_remote_state.networking.outputs.firewall_id
   pools {
     label      = "nodes"
     size       = element(data.civo_size.xsmall.sizes, 0).name
