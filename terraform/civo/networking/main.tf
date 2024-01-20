a4101ec90624e1ba96a91d9d9ae41559208fc5c9
Ivan Lee <ivanklee86@gmail.com>
2024-01-20 07:25:46 +0000

    Changes?

---
+++ terraform/civo/networking/main.tf
@@ -0,0 +1,25 @@
+resource "civo_network" "lab" {
+  label = "lab"
+}
+
+resource "civo_firewall" "lab_firewall" {
+  name = "lab_firewall"
+  network_id = civo_network.lab.id
+  
+  ingress_rule {
+    label      = "k8s"
+    protocol   = "tcp"
+    port_range = "6443"
+    cidr       = ["100.8.82.42/32"]
+    action     = "allow"
+  }
+
+  egress_rule {
+    label      = "all"
+    protocol   = "tcp"
+    port_range = "1-65535"
+    cidr       = ["0.0.0.0/0"]
+    action     = "allow"
+  }
+
+}
