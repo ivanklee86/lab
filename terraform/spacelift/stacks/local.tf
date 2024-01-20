37069e31e8a7d2e3c75966e5f00570bf77cd33e5
Ivan Lee <ivanklee86@gmail.com>
2024-01-20 07:44:28 +0000

    Clean up tags and prep for proxied-via-cloudflare.

--- terraform/spacelift/stacks/local.tf
+++ terraform/spacelift/stacks/local.tf
@@ -4,13 +4,13 @@ locals {
       name : "dns/ivanlee.me"
       description : "DNS for ivanlee.me."
       project_root : "terraform/cloudflare/dns/ivanlee.me"
-      labels : ["cloudflare", "dns"]
+      labels : ["cloudflare"]
     },
     {
       name : "civo/networking"
       description : "CIVO networking"
       project_root : "terraform/civo/networking"
-      labels : ["civo"]
+      labels : ["civo", "cloudflare"]
     },
   ]
 }
