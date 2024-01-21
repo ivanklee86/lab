ca2b67c6ed53fc5995203746ad4ac0ee45786a39
Ivan Lee <ivanklee86@gmail.com>
2024-01-21 08:49:21 +0000

    Add possibility of using Terraform.

--- terraform/digitalocean/project/terraform.tf
+++ terraform/digitalocean/project/terraform.tf
@@ -1,7 +1,7 @@
 terraform {
   required_providers {
     digitalocean = {
-      source = "opentofu/digitalocean"
+      source = "digitalocean/digitalocean"
       version = "~> 2.0"
     }
   }
--- terraform/spacelift/stacks/local.tf
+++ terraform/spacelift/stacks/local.tf
@@ -1,10 +1,13 @@
 locals {
+  opentofu_version = "1.6.0"
+  terraform_version = "1.5.7"
   stacks = [
     {
       name : "dns/ivanlee.me"
       description : "DNS for ivanlee.me."
       project_root : "terraform/cloudflare/dns/ivanlee.me"
       labels : ["cloudflare"]
+      terraform_workflow_tool: "OPEN_TOFU"
       terraform_external_state_access : false
     },
     {
@@ -12,6 +15,7 @@ locals {
       description : "DigitalOcean project"
       project_root : "terraform/digitalocean/project"
       labels : ["digitalocean"]
+      terraform_workflow_tool: "TERRAFORM_FOSS"
       terraform_external_state_access : true
     },
   ]
