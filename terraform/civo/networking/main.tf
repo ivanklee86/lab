06d608b8510f4c552a0d619e647053e9953514c1
Ivan Lee <ivanklee86@gmail.com>
2024-01-21 09:34:47 +0000

    Rule poc.

--- terraform/civo/networking/main.tf
+++ terraform/civo/networking/main.tf
@@ -11,39 +11,47 @@ resource "civo_network" "lab" {
 resource "civo_firewall" "lab_firewall" {
   name                 = "lab_firewall"
   network_id           = civo_network.lab.id
-  create_default_rules = false
+  create_default_rules = true
 
-  ingress_rule {
-    label      = "k8s"
-    protocol   = "tcp"
-    port_range = "6443"
-    cidr       = ["0.0.0.0/0"]
-    action     = "allow"
-  }
+    # ingress_rule {
+  #   label      = "k8s"
+  #   protocol   = "tcp"
+  #   port_range = "6443"
+  #   cidr       = ["0.0.0.0/0"]
+  #   action     = "allow"
+  # }
 
-  ingress_rule {
-    label      = "ssh"
-    protocol   = "tcp"
-    port_range = "22"
-    cidr       = ["0.0.0.0/0"]
-    action     = "allow"
-  }
+  # ingress_rule {
+  #   label      = "k8s"
+  #   protocol   = "tcp"
+  #   port_range = "6443"
+  #   cidr       = ["0.0.0.0/0"]
+  #   action     = "allow"
+  # }
 
-  ingress_rule {
-    label      = "http"
-    protocol   = "tcp"
-    port_range = "80"
-    cidr       = concat(data.cloudflare_ip_ranges.cloudflare.cidr_blocks, formatlist(local.home_ip))
-    action     = "allow"
-  }
+  # ingress_rule {
+  #   label      = "ssh"
+  #   protocol   = "tcp"
+  #   port_range = "22"
+  #   cidr       = ["0.0.0.0/0"]
+  #   action     = "allow"
+  # }
 
-  ingress_rule {
-    label      = "https"
-    protocol   = "tcp"
-    port_range = "443"
-    cidr       = concat(data.cloudflare_ip_ranges.cloudflare.cidr_blocks, formatlist(local.home_ip))
-    action     = "allow"
-  }
+  # ingress_rule {
+  #   label      = "http"
+  #   protocol   = "tcp"
+  #   port_range = "80"
+  #   cidr       = concat(data.cloudflare_ip_ranges.cloudflare.cidr_blocks, formatlist(local.home_ip))
+  #   action     = "allow"
+  # }
+
+  # ingress_rule {
+  #   label      = "https"
+  #   protocol   = "tcp"
+  #   port_range = "443"
+  #   cidr       = concat(data.cloudflare_ip_ranges.cloudflare.cidr_blocks, formatlist(local.home_ip))
+  #   action     = "allow"
+  # }
 
   egress_rule {
     label      = "all"
