87926a508bfdc6e3047e4cc31b82d8e7a6f35da3
Ivan Lee <ivanklee86@gmail.com>
2024-01-22 21:23:16 +0000

    Only use gpg when running locally.

--- .devcontainer/post_install.sh
+++ .devcontainer/post_install.sh
@@ -14,3 +14,15 @@ poetry install --no-root
 
 # pre-commit
 poetry run pre-commit install
+
+# Set up GPG if not codespaces
+if [ "$CODESPACES" != "true" ]; then
+    echo "Running locally, configure gpg."
+
+    git config --global gpg.program gpg2
+    git config --global user.signingkey ivanklee86@gmail.com
+    git config --global commit.gpgsign true
+    git config --global push.autoSetupRemote true
+else
+    echo "Running in codespaces."
+fi
