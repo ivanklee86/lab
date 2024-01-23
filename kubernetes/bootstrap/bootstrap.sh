058fd45d15150374885cb93dd4a4945365537ce6
Ivan Lee <ivanklee86@gmail.com>
2024-01-23 05:12:01 -0500

    ArgoCD setup (#38)
    * Set up ArgoCD chart.
    
    * Set up ArgoCD and application deployments.

--- kubernetes/bootstrap/bootstrap.sh
+++ kubernetes/bootstrap/bootstrap.sh
@@ -12,6 +12,13 @@ kubectl apply -f onepassword-token.yaml
 op inject -i op-credentials.yaml.tmpl -o op-credentials.yaml
 kubectl apply -f op-credentials.yaml
 
-# Template helm
+# Template 1PW operator
 helm template --namespace infrastructure --include-crds ../applications/infrastructure/connect > manifests.yaml
 kubectl apply -f manifests.yaml
+
+# Template ArgoCD
+helm template --release-name argocd --namespace infrastructure ../applications/infrastructure/argocd > manifests.yaml
+kubectl apply -f manifests.yaml
+
+# Bootstrap applications
+kubectl apply -f application.yaml
