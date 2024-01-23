10d67d1b97c0964c8cb31abfc6042b95ffde745f
Ivan Lee <ivanklee86@gmail.com>
2024-01-23 12:36:28 -0500

    Increase k8s node size. (#43)

--- terraform/civo/kubernetes/main.tf
+++ terraform/civo/kubernetes/main.tf
@@ -5,7 +5,7 @@ resource "civo_kubernetes_cluster" "lab" {
   firewall_id        = data.terraform_remote_state.networking.outputs.firewall_id
   pools {
     label      = "nodes"
-    size       = element(data.civo_size.xsmall.sizes, 0).name
+    size       = element(data.civo_size.kubernetes_sizes.sizes, 1).name
     node_count = 3
   }
 }
