9a5a4d34eefc9c48467429d4d2925b75fbf408e3
Ivan Lee <ivanklee86@gmail.com>
2024-01-23 05:54:28 -0500

    Set up aoach.tech Terraform. (#41)
    * Set up aoach.tech Terraform.
    
    * Remove additional record.
    
    * Update formatting.
    
    * Add firewall ID to load balancer.

---
+++ terraform/cloudflare/dns/aoach.tech/main.tf
@@ -0,0 +1,9 @@
+module "dns-website" {
+  source = "../../../../terraform_modules/cloudflare/dns"
+
+  domain_name        = "aoach.tech"
+  ip                 = "212.2.240.67"
+  additional_records = []
+  proxied            = true
+  ttl                = 1
+}
