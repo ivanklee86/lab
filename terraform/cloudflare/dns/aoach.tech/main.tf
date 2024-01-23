c19dc5ac27956fbd57556a992c3701c414f304c9
Ivan Lee <ivanklee86@gmail.com>
2024-01-23 10:51:24 +0000

    Update formatting.

--- terraform/cloudflare/dns/aoach.tech/main.tf
+++ terraform/cloudflare/dns/aoach.tech/main.tf
@@ -1,9 +1,9 @@
 module "dns-website" {
   source = "../../../../terraform_modules/cloudflare/dns"
 
-  domain_name = "aoach.tech"
-  ip          = "212.2.240.67"
+  domain_name        = "aoach.tech"
+  ip                 = "212.2.240.67"
   additional_records = []
-  proxied = true
-  ttl     = 1
+  proxied            = true
+  ttl                = 1
 }
