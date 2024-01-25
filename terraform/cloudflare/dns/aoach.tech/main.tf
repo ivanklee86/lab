169c48adc394c0f8164789d387f9d3921e85f8be
Ivan Lee <ivanklee86@gmail.com>
2024-01-25 07:49:57 +0000

    Modify Cloudflare module to set up A records for lab correctly.

--- terraform/cloudflare/dns/aoach.tech/main.tf
+++ terraform/cloudflare/dns/aoach.tech/main.tf
@@ -3,7 +3,18 @@ module "dns-website" {
 
   domain_name        = "aoach.tech"
   ip                 = "212.2.240.67"
-  additional_records = []
+  additional_records = [
+    {
+      type  = "A"
+      name  = "whoami"
+      value = ""
+    },
+    {
+      type  = "A"
+      name  = "argocd"
+      value = ""
+    },
+  ]
   proxied            = true
   ttl                = 1
 }
