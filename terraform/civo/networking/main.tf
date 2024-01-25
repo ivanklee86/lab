0dc5ce6ceaece2f57e1cd6956e399608ee8a2a2a
Ivan Lee <ivanklee86@gmail.com>
2024-01-25 22:25:30 +0000

    Just one firewall.

--- terraform/civo/networking/main.tf
+++ terraform/civo/networking/main.tf
@@ -68,69 +68,6 @@ resource "civo_firewall" "lab_k8s_firewall" {
   }
 }
 
-resource "civo_firewall" "lab_firewall" {
-  name                 = "lab_lb_firewall"
-  network_id           = civo_network.lab.id
-  create_default_rules = false
-
-  ingress_rule {
-    label      = "k8s"
-    protocol   = "tcp"
-    port_range = "6443"
-    cidr       = ["0.0.0.0/0"]
-    action     = "allow"
-  }
-
-  ingress_rule {
-    label      = "ping/traceroute"
-    protocol   = "icmp"
-    port_range = "N/A"
-    cidr       = ["0.0.0.0/0"]
-    action     = "allow"
-  }
-
-  ingress_rule {
-    label      = "http"
-    protocol   = "tcp"
-    port_range = "80"
-    cidr       = concat(data.cloudflare_ip_ranges.cloudflare.cidr_blocks, formatlist(local.home_ip))
-    action     = "allow"
-  }
-
-  ingress_rule {
-    label      = "https"
-    protocol   = "tcp"
-    port_range = "443"
-    cidr       = concat(data.cloudflare_ip_ranges.cloudflare.cidr_blocks, formatlist(local.home_ip))
-    action     = "allow"
-  }
-
-  egress_rule {
-    label      = "All"
-    protocol   = "tcp"
-    port_range = "1-65535"
-    cidr       = ["0.0.0.0/0"]
-    action     = "allow"
-  }
-
-  egress_rule {
-    label      = "All"
-    protocol   = "udp"
-    port_range = "1-65535"
-    cidr       = ["0.0.0.0/0"]
-    action     = "allow"
-  }
-
-  egress_rule {
-    label      = "Ping/Traceroute"
-    protocol   = "icmp"
-    port_range = "N/A"
-    cidr       = ["0.0.0.0/0"]
-    action     = "allow"
-  }
-
-}
-
 resource "civo_reserved_ip" "ingress" {
   name = "ingress"
 }
