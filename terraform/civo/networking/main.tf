a0bc633609e8f385ae0ca594886d7963a01875d6
Ivan Lee <ivanklee86@gmail.com>
2024-01-25 22:34:15 +0000

    Check firewall again.

--- terraform/civo/networking/main.tf
+++ terraform/civo/networking/main.tf
@@ -32,8 +32,8 @@ resource "civo_firewall" "lab_k8s_firewall" {
     label      = "http"
     protocol   = "tcp"
     port_range = "80"
-    # cidr       = concat(data.cloudflare_ip_ranges.cloudflare.cidr_blocks, formatlist(local.home_ip))
-    cidr       = ["0.0.0.0/0"]
+    cidr       = concat(data.cloudflare_ip_ranges.cloudflare.ipv4_cidr_blocks, formatlist(local.home_ip))
+    # cidr       = ["0.0.0.0/0"]
     action     = "allow"
   }
 
@@ -41,8 +41,8 @@ resource "civo_firewall" "lab_k8s_firewall" {
     label      = "https"
     protocol   = "tcp"
     port_range = "443"
-    # cidr       = concat(data.cloudflare_ip_ranges.cloudflare.cidr_blocks, formatlist(local.home_ip))
-    cidr       = ["0.0.0.0/0"]
+    cidr       = concat(data.cloudflare_ip_ranges.cloudflare.ipv4_cidr_blocks, formatlist(local.home_ip))
+    # cidr       = ["0.0.0.0/0"]
     action     = "allow"
   }
 
