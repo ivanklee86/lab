52ef3fdce3e69c263b3871aa98ebaa85f413fca9
Ivan Lee <ivanklee86@gmail.com>
2024-01-28 13:43:39 -0500

    Update diagrams for latest learnings. (#76)

--- docs/diagrams/tooling.py
+++ docs/diagrams/tooling.py
@@ -21,7 +21,6 @@ with Diagram("Tooling", filename=diagram_filename, outformat=["png"], show=False
     with Cluster("Github.com"):
         github = Github("Github")
         github_actions = Custom("Github Actions", "../imgs/github_actions.png")
-        cloud_dev_container = Custom("DevContainer [Cloud]", "../imgs/devcontainer.png")
 
     dev_container >> github >> Edge(label="CI/CD") >> github_actions
     github >> Edge(label="IaC") >> spacelift
