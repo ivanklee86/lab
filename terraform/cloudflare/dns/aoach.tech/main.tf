4cfa75f2881abd6b03651c8d8c19c232b20b9c4e
Ivan Lee <ivanklee86@gmail.com>
2024-01-31 06:20:40 +0000

    Escape slash?

--- terraform/cloudflare/dns/aoach.tech/main.tf
+++ terraform/cloudflare/dns/aoach.tech/main.tf
@@ -1,5 +1,5 @@
 module "dns-website" {
-  source = "github.com/ivanklee86/lab//terraform_modules/cloudflare/dns?ref=tm/v0.0.1"
+  source = "github.com/ivanklee86/lab//terraform_modules/cloudflare/dns?ref=tm\\/v0.0.1"
 
   domain_name = "aoach.tech"
   ip          = "212.2.240.67"
